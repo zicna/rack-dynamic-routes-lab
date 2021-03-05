de5e7ef5cbecb4a980db7aec1881bec19dc88278
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-03-04 22:57:45 -0600

    Done.

---
+++ app/application.rb
@@ -0,0 +1,21 @@
+class Application
+    def call(env)
+        response = Rack::Response.new
+        request = Rack::Request.new(env)
+        if request.path.match(/items/)
+            item_name = request.path.split("/items/").last
+           item = @@items.find{|i| i.name == item_name}
+           if item
+           response.write item.price
+           else
+            response.write "Item not found"
+            response.status = 400
+           end
+
+        else 
+            response.write "Route not found"
+            response.status = 404
+        end
+        response.finish
+    end
+end
--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,9 @@
 class Item
   attr_accessor :name, :price
-
+  @@item = []
   def initialize(name,price)
     @name = name
     @price = price
+    @@item << self
   end
 end
